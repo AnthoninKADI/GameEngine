049b93b8af5f349ec89f1d4be2c821a6ee3e6e5d
Anthonin <akadi@artfx.fr>
2024-03-26 12:31:18 +0100

    [Add] paddle, walls, restart, collision

--- PersProjGameEngine/Game.cpp
+++ PersProjGameEngine/Game.cpp
@@ -5,186 +5,110 @@ using namespace std;
 
 bool Game::initialize()
 {
-	bool isWindowInit = window.initialize();
-	bool isRendererInit = renderer.initialize(window);
+    bool isWindowInit = window.initialize();
+    bool isRendererInit = renderer.initialize(window);
 
-	int windowWidth = window.getWidth();
-	int windowHeight = window.getHeight();
+    int windowWidth = window.getWidth();
+    int windowHeight = window.getHeight();
 
-	topWall = { 0,0, static_cast<float>(windowWidth), wallThickness };
-	bottomWall = { 0, windowHeight - wallThickness, static_cast<float>(windowWidth), wallThickness };
-	rightWall = { windowWidth - wallThickness, 0, wallThickness, static_cast<float>(windowHeight) };
+    topWall = { 0,0, static_cast<float>(windowWidth), wallThickness };
+    rightWall = { windowWidth - wallThickness, 0, wallThickness, static_cast<float>(windowHeight) };
+    leftWall = { 0, 0, wallThickness, static_cast<float>(windowHeight) }; 
 
-	return isWindowInit && isRendererInit; // Return bool && bool && .. to detect error
+    return isWindowInit && isRendererInit;
 }
 
 void Game::loop()
 {
-	Timer timer;
-	float dt = 0;
-	while (isRunning)
-	{
-		float dt = timer.computeDeltaTime() / 1000.0f;
-		processInput();
-		update(dt);
-		render();
-		timer.delayTime();
-	}
+    Timer timer;
+    while (isRunning)
+    {
+        float dt = timer.computeDeltaTime() / 1000.0f;
+        processInput();
+        update(dt);
+        render();
+        timer.delayTime();
+    }
 }
 
 void Game::close()
 {
-	renderer.close();
-	window.close();
-	SDL_Quit();
+    renderer.close();
+    window.close();
+    SDL_Quit();
 }
 
 void Game::processInput()
 {
-	// SDL Event
-	SDL_Event event;
-	while (SDL_PollEvent(&event))
-	{
-		switch (event.type)
-		{
-		case SDL_QUIT:
-			isRunning = false;
-			break;
-		}
-	}
-	// Keyboard state
-	const Uint8* keyboardState = SDL_GetKeyboardState(nullptr);
-	// Escape: quit game
-	if (keyboardState[SDL_SCANCODE_ESCAPE])
-	{
-		isRunning = false;
-	}
-	// Paddle move Left
-	if (keyboardState[SDL_SCANCODE_W])
-	{
-		paddleDirectionLeft = -1;
-	}
-	else if (keyboardState[SDL_SCANCODE_S])
-	{
-		paddleDirectionLeft = 1;
-	}
-	else 
-	{
-		paddleDirectionLeft = 0;
-	}
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+        case SDL_QUIT:
+            isRunning = false;
+            break;
+        }
+    }
 
-	// Paddle move Right
-	if (keyboardState[SDL_SCANCODE_UP])
-	{
-		paddleDirectionRight = -1;
-	}
-	else if (keyboardState[SDL_SCANCODE_DOWN])
-	{
-		paddleDirectionRight = 1;
-	}
-	else 
-	{
-		paddleDirectionRight = 0;
-	}
+    const Uint8* keyboardState = SDL_GetKeyboardState(nullptr);
 
-	memcpy(prevKeyboardStateLeft, keyboardState, SDL_NUM_SCANCODES * sizeof(Uint8));
-	memcpy(prevKeyboardStateRight, keyboardState, SDL_NUM_SCANCODES * sizeof(Uint8));
+    // Déplacement continu du paddle de gauche à droite
+    if (keyboardState[SDL_SCANCODE_LEFT])
+    {
+        paddlePos.x -= paddleVelocity.x;
+        if (paddlePos.x < wallThickness)
+            paddlePos.x = wallThickness;
+    }
+    else if (keyboardState[SDL_SCANCODE_RIGHT])
+    {
+        paddlePos.x += paddleVelocity.x;
+        if (paddlePos.x + paddleWidth > window.getWidth() - wallThickness)
+            paddlePos.x = window.getWidth() - paddleWidth - wallThickness;
+    }
 }
 
 void Game::update(float dt)
 {
-	// Paddle move left
-	paddlePosLeft += paddleVelocityLeft * dt * paddleDirectionLeft;
-	if (paddlePosLeft.y < paddleHeight / 2 + wallThickness)
-	{
-		paddlePosLeft.y = paddleHeight / 2 + wallThickness;
-	}
-	if (paddlePosLeft.y > window.getHeight() - paddleHeight / 2 - wallThickness)
-	{
-		paddlePosLeft.y = window.getHeight() - paddleHeight / 2 - wallThickness;
-	}
+    ballPos += ballVelocity * dt;
 
-	// Paddle move right
-	paddlePosRight += paddleVelocityRight * dt * paddleDirectionRight;
-	if (paddlePosRight.y < paddleHeight / 2 + wallThickness)
-	{
-		paddlePosRight.y = paddleHeight / 2 + wallThickness;
-	}
-	if (paddlePosRight.y > window.getHeight() - paddleHeight / 2 - wallThickness)
-	{
-		paddlePosRight.y = window.getHeight() - paddleHeight / 2 - wallThickness;
-	}
+    if (ballPos.y < wallThickness) 
+    {
+        ballVelocity.y *= -1; 
+    }
+    if (ballPos.x < wallThickness || ballPos.x > window.getWidth() - wallThickness)
+    {
+        ballVelocity.x *= -1;
+    }
 
-	//Ball move
-	ballPos += ballVelocity * dt;
+    // Collision avec le paddle
+    if (ballPos.y + ballSize / 2 > paddlePos.y && ballPos.y - ballSize / 2 < paddlePos.y + paddleHeight &&
+        ballPos.x + ballSize / 2 > paddlePos.x && ballPos.x - ballSize / 2 < paddlePos.x + paddleWidth)
+    {
+        ballVelocity.y *= -1;
+    }
 
-	// Ball collision with top and bottom walls
-	if (ballPos.y < ballSize / 2 + wallThickness || ballPos.y > window.getHeight() - ballSize / 2 - wallThickness)
-	{
-		ballVelocity.y *= -1;
-		
-		if (ballPos.y < ballSize / 2 + wallThickness)
-			ballPos.y = ballSize / 2 + wallThickness;
-		else
-			ballPos.y = window.getHeight() - ballSize / 2 - wallThickness;
-	}
-
-	// Ball collision with left and right walls
-	if (ballPos.x < 0 || ballPos.x > window.getWidth())
-	{
-		// Restart the game
-		ballVelocity = { 250, 0 }; 
-		ballPos = { window.getWidth() / 2.f, window.getHeight() / 2.f };
-	}
-
-	// Ball-Paddle collision (left paddle)
-	Vector2 leftDiff = ballPos - paddlePosLeft;
-	if (fabsf(leftDiff.y) <= paddleHeight / 2
-		&& fabsf(leftDiff.x) <= paddleWidth / 2 + ballSize / 2
-		&& ballVelocity.x < 0)
-	{
-		ballVelocity.x *= -1;
-		ballPos.x = paddlePosLeft.x + paddleWidth / 2 + ballSize / 2;
-	}
-
-	// Ball-Paddle collision (right paddle)
-	Vector2 rightDiff = ballPos - paddlePosRight;
-	if (fabsf(rightDiff.y) <= paddleHeight / 2
-		&& fabsf(rightDiff.x) <= paddleWidth / 2 + ballSize / 2
-		&& ballVelocity.x > 0) 
-	{
-		ballVelocity.x *= -1;
-		ballPos.x = paddlePosRight.x - paddleWidth / 2 - ballSize / 2; 
-	}
-
-	//Restart automatically
-	if (ballPos.x < 0) {
-		ballVelocity.x *= -1;
-		ballPos.x = window.getWidth() / 2.f;
-		ballPos.y = window.getHeight() / 2.f;
-	}
-	else if (ballPos.x > window.getWidth() - ballSize / 2) {
-		ballVelocity.x *= -1;
-		ballPos.x = window.getWidth() / 2.f;
-		ballPos.y = window.getHeight() / 2.f;
-	}
+    // Réinitialisation 
+    if (ballPos.y > window.getHeight())
+    {
+        ballPos = { window.getWidth() / 2.f, window.getHeight() / 2.f };
+        ballVelocity = { 250, 250 };
+    }
 }
 
 void Game::render()
 {
-	renderer.beginDraw();
+    renderer.beginDraw();
 
-	renderer.drawRect(topWall);
-	renderer.drawRect(bottomWall);
+    renderer.drawRect(topWall);
+    renderer.drawRect(rightWall);
+    renderer.drawRect(leftWall);
 
-	Rectangle ballRect = { ballPos.x - ballSize / 2, ballPos.y - ballSize / 2, ballSize, ballSize };
-	renderer.drawRect(ballRect);
+    Rectangle ballRect = { ballPos.x - ballSize / 2, ballPos.y - ballSize / 2, ballSize, ballSize };
+    renderer.drawRect(ballRect);
 
-	Rectangle paddleRectLeft = { paddlePosLeft.x - paddleWidth / 2, paddlePosLeft.y - paddleHeight / 2, paddleWidth, paddleHeight };
-	renderer.drawRect(paddleRectLeft);
+    Rectangle paddleRect = { paddlePos.x, paddlePos.y, paddleWidth, paddleHeight };
+    renderer.drawRect(paddleRect);
 
-	Rectangle paddleRectRight = { paddlePosRight.x - paddleWidth / 2, paddlePosRight.y - paddleHeight / 2, paddleWidth, paddleHeight };
-	renderer.drawRect(paddleRectRight);
-
-	renderer.endDraw();
+    renderer.endDraw();
 }
--- PersProjGameEngine/Game.h
+++ PersProjGameEngine/Game.h
@@ -6,69 +6,56 @@
 class Game
 {
 public:
-	static Game& instance()
-	{
-		static Game inst;
-		return inst;
-	}
+    static Game& instance()
+    {
+        static Game inst;
+        return inst;
+    }
 
-	Game(const Game&) = delete;
-	Game& operator=(const Game&) = delete;
-	Game(Game&&) = delete;
-	Game& operator=(Game&&) = delete;
+    Game(const Game&) = delete;
+    Game& operator=(const Game&) = delete;
+    Game(Game&&) = delete;
+    Game& operator=(Game&&) = delete;
 
 private:
-	Game():
-		isRunning(true),
-		ballPos({100, 100}),
-		ballVelocity({250, 250}),
-		paddlePosLeft({50, 100}), //Left paddle
-		paddleVelocityLeft({0, 450}),
-		paddleDirectionLeft(0),
-		paddlePosRight({950, 100}), //Right paddle
-		paddleVelocityRight({0, 450}),
-		paddleDirectionRight(0),	
-		wallThickness(10),
-		topWall(Rectangle()),
-		bottomWall(Rectangle()),
-		rightWall(Rectangle())
-	{}
+    Game() :
+        isRunning(true),
+        ballPos({ 100, 100 }),
+        ballVelocity({ 250, 250 }),
+        paddlePos({ 400, 720 }), 
+        paddleVelocity({ 10, 0 }), 
+        wallThickness(10),
+        topWall(Rectangle()),
+        rightWall(Rectangle()),
+        leftWall(Rectangle())
+    {}
 
 public:
-	bool initialize();
-	void loop();
-	void close();
+    bool initialize();
+    void loop();
+    void close();
 
 private:
-	void processInput();
-	void update(float dt);
-	void render();
+    void processInput();
+    void update(float dt);
+    void render();
 
-	bool isRunning;
-	Window window;
-	Renderer renderer;
+    bool isRunning;
+    Window window;
+    Renderer renderer;
 
-	Rectangle topWall;
-	Rectangle bottomWall;
-	Rectangle rightWall;
-	const float wallThickness = 10;
+    Rectangle topWall;
+    Rectangle rightWall;
+    Rectangle leftWall;
+    const float wallThickness;
 
-	Vector2 ballPos;
-	Vector2 ballVelocity;
-	const float ballSize = 10;
+    Vector2 ballPos;
+    Vector2 ballVelocity;
+    const float ballSize = 10;
 
-	Vector2 paddlePosLeft;
-	Vector2 paddleVelocityLeft;
-	float paddleDirectionLeft;
-	
-	Vector2 paddlePosRight;
-	Vector2 paddleVelocityRight;
-	float paddleDirectionRight;
-
-	const float paddleWidth = 10;
-	const float paddleHeight = 64;
-
-	bool prevKeyboardStateLeft[SDL_NUM_SCANCODES] = { false };
-	bool prevKeyboardStateRight[SDL_NUM_SCANCODES] = { false };
+    Vector2 paddlePos;
+    Vector2 paddleVelocity;
+    const float paddleWidth = 100; 
+    const float paddleHeight = 20; 
 };
 
